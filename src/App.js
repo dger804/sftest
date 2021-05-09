8a943b088bea2f6270f9de8a63574eab69f26b5b
dger804 <diego804@gmail.com>
2021-05-09 10:03:29 -0500

    Adding react router dom, page and navigation component with routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router,
+  Route, 
+} from 'react-router-dom'
+import { Navigation } from './components/Navigation'
+import { Home } from './pages/Home'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      </header>
+        <Router>
+          <Navigation/>
+          <Route path="/" component={Home}/>
+        </Router>
+      </header>      
     </div>
   );
 }
